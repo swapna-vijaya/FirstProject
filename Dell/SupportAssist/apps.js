fde1ab6a955257455b4fac154cb112faaae63a1f
swapna-vijaya <vijayaswapnasiric@gmail.com>
2024-08-29 15:20:02 +0530

    Myallproject

---
+++ Dell/SupportAssist/apps.js
@@ -0,0 +1,9 @@
+let heading=React.createElement(
+    'h1',{
+        id:"bg-red"
+    },
+    'This is h1 tag'
+);
+let ele=document.getElementById('root');
+let root = ReactDOM.createRoot(ele);
+root.render(heading);
---
+++ app.js
@@ -0,0 +1,11 @@
+let heading=React.createElement(
+    'h1',{
+        id:"bg-blue"
+    },
+    'The plot centers around a theme park on a remote island where genetically engineered dinosaurs are brought to life using DNA extracted from prehistoric  insects preserved in amber.The parks visionary creator,JohnHammond,invites a group of experts-including paleontalogists Dr.Alan Grant and Dr.Ellie Sattler,along with chaos theorist Dr.Ian Malcolm-to certify the parks safety before it opens to the public.Paleontology,the study of ancient life,continues to uncover new dinosaur species and provide insights into their biology and evolution.Technological advancements,such as CT Scanning and computer modelling,allow scientists to reconstruct the dinosaurs apperances and behaviors with increasing accracy. '
+    
+    
+);
+let ele=document.getElementById('array');
+let root = ReactDOM.createRoot(ele);
+root.render(heading);
